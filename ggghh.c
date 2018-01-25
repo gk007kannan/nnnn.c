efc3a15248389adb55f55c34fcc42da0d775e692
gk007kannan <35525377+gk007kannan@users.noreply.github.com>
2018-01-25 15:04:11 +0530

    Create ggghh.c

---
+++ ggghh.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+#include<conio.h>
+# define bool int
+void main();
+{
+int l, r;
+quickSort(A, 0, arr_size-1);
+1 = 0;
+r = arr_size-1; 
+while (l < r)
+{
+if(A[l] + A[r] == sum)
+return 1; 
+if(A[l] + A[r] < sum)
+l++;
+A[i] + A[j] > sum
+r--;
+}    
+getch();
+}
